847bf9ca32ad3bfb5b5634f1db8a36b1093a7c8a
clariceoliveira <claricedeoliv7@gmail.com>
2023-08-27 15:17:36 -0300

    muda imagem

---
+++ mudaimagem.js
@@ -0,0 +1,12 @@
+function mudaimagem(){
+    var primimg = document.querySelector("#primeiraimg");
+    var segimg= document.querySelector("#segundaimg");
+    primimg.style.display= "none";
+    segimg.style.display= "block";
+}
+function voltaimagem(){
+    var primimg = document.querySelector("#primeiraimg");
+    var segimg= document.querySelector("#segundaimg");
+    primimg.style.display= "block";
+    segimg.style.display= "none";
+}
